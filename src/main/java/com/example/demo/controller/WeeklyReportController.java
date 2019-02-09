a3bd95bed2c1470067cfa78c1b47806c022cc765
banana5388 <banana5388@gmail.com>
2019-02-09 18:28:34 +0900

    feat: カレンダーのヘッダー表示までを実装

--- src/main/java/com/example/demo/controller/WeeklyReportController.java
+++ src/main/java/com/example/demo/controller/WeeklyReportController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.CalendarModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -12,7 +16,13 @@ import java.util.Map;
 public class WeeklyReportController {
 
     @RequestMapping("/init")
-    public String init() {
+    public String init(Model model) {
+
+        // 現在日付の作成
+        List<String> dayOfWeekList = CalendarModel.getDayOfWeek(CalendarModel.START_MONDAY);
+
+        model.addAttribute("dayOfWeekList", dayOfWeekList);
+
         return "weeklyReport";
     }
 
---
+++ src/main/java/com/example/demo/model/CalendarModel.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarModel {
+
+    public static final int START_SUNDAY = 0;
+    public static final int START_MONDAY = 1;
+
+    public static List<String> getDayOfWeek(int startDayOfWeek) {
+        List<String> dayOfWeekList = new ArrayList<String>();
+
+        switch (startDayOfWeek) {
+            case START_SUNDAY:
+            default:
+                dayOfWeekList.add("Sun");
+                dayOfWeekList.add("Mon");
+                dayOfWeekList.add("Tue");
+                dayOfWeekList.add("Wed");
+                dayOfWeekList.add("Thu");
+                dayOfWeekList.add("Fri");
+                dayOfWeekList.add("Sut");
+        }
+
+        return dayOfWeekList;
+    }
+}
