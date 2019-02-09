c16be720943333ea23000079a25dd49cdbd71974
banana5388 <banana5388@gmail.com>
2019-02-09 17:24:33 +0900

    feat: uikitの追加

--- src/main/java/com/example/demo/controller/WeeklyReportController.java
+++ src/main/java/com/example/demo/controller/WeeklyReportController.java
@@ -2,23 +2,23 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping("/weeklyReport")
-public class WeeklyReport {
+public class WeeklyReportController {
 
     @RequestMapping("/init")
-    public String init(Model model) {
+    public String init() {
         return "weeklyReport";
     }
 
-    @RequestMapping("doPost")
-    public String doPost(Model model) {
-        Map<String, Object> modelMap = model.asMap();
-
+    @RequestMapping("/post")
+    public String doPost(@RequestBody String body, Model model) {
+        model.addAttribute("text", body);
         return "weeklyReport";
     }
 }
